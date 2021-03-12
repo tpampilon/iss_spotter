f52245a8fa3a7b77cebd3290d899dcba08047432
TedP <ted.pampilon@gmail.com>
2021-03-12 04:59:42 +0000

    added index2.js and iss-promised.js

---
+++ index2.js
@@ -0,0 +1,20 @@
+const fetchMyIP = require('./iss-promised');
+const request = require('request-promise-native');
+const iss = require('./iss-promised');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+iss.nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
